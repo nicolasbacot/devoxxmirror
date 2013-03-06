2c3e9418f52005d8caf96d4cbd16194990b938df
Benoit Prioux <benoit.prioux@gmail.com>
2013-03-06 21:51:16 +0100

    add startHour and durationHour for timetable

--- src/test/java/com/devoxx/model/ScheduleSessionTest.java
+++ src/test/java/com/devoxx/model/ScheduleSessionTest.java
@@ -46,4 +46,47 @@ public class ScheduleSessionTest {
 
         assertThat(session.getDurationMinute()).isEqualTo("180");
     }
+
+    @Test
+    public void durationHour_should_return_duration_in_hour() {
+        ScheduledSession session = new ScheduledSession();
+        session.setFromTime("2013-03-27 09:30:00.0");
+        session.setToTime("2013-03-27 12:30:00.0");
+
+        assertThat(session.getDurationHour()).isEqualTo("3.00000");
+    }
+
+    @Test
+    public void durationHour_should_return_duration_in_hour_with_decimal() {
+        ScheduledSession session = new ScheduledSession();
+        session.setFromTime("2013-03-27 09:30:00.0");
+        session.setToTime("2013-03-27 10:00:00.0");
+
+        assertThat(session.getDurationHour()).isEqualTo("0.50000");
+    }
+
+    @Test
+    public void durationHour_should_return_duration_in_hour_with_5decimal() {
+        ScheduledSession session = new ScheduledSession();
+        session.setFromTime("2013-03-27 10:10:00.0");
+        session.setToTime("2013-03-27 10:45:00.0");
+
+        assertThat(session.getDurationHour()).isEqualTo("0.58333");
+    }
+
+    @Test
+    public void startHour_should_return_0_from_start() {
+        ScheduledSession session = new ScheduledSession();
+        session.setFromTime("2013-03-27 07:00:00.0");
+
+        assertThat(session.getStartHour()).isEqualTo("0.00000");
+    }
+
+    @Test
+    public void startHour_should_return_duration_from_start() {
+        ScheduledSession session = new ScheduledSession();
+        session.setFromTime("2013-03-27 10:00:00.0");
+
+        assertThat(session.getStartHour()).isEqualTo("3.00000");
+    }
 }
